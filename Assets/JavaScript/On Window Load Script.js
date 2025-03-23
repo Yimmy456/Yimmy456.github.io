3efc1af5acb51b417b3a2b876e27ad390c87ac73
ManarAjhar <manarajhar@hotmail.com>
2025-03-23 22:51:36 +0400

    Update 13
    - I changed the height of the slideshow description's text container to 85%;
    
    - I removed a duplicate JavaScript file;
    
    - I set the default toggle status of the "Experience" panels to hide (even for the slideshows);
    
    - I added more content for the slideshow of the first project (However, the 'ul' and 'li' tags for each content needs to be fixed).

--- Assets/JavaScript/On Window Load Script.js
+++ Assets/JavaScript/On Window Load Script.js
@@ -2,17 +2,17 @@ window.onload= function()
 {
   showDivs(1, 0);
 
-  HideOrShowDesc2('Education', '1', '1');
-  HideOrShowDesc2('Education', '2', '1');
-  HideOrShowDesc2('Education', '3', '1');
+  HideOrShowDesc2('Education', '1', '0');
+  HideOrShowDesc2('Education', '2', '0');
+  HideOrShowDesc2('Education', '3', '0');
 
-  HideOrShowDesc2('Project', '1', '1');
+  HideOrShowDesc2('Project', '1', '0');
 
-  HideOrShowDesc2('Award', '1', '1');
+  HideOrShowDesc2('Award', '1', '0');
 
-  HideOrShowDesc2('PE', '1', '1');
-  HideOrShowDesc2('PE', '2', '1');
-  HideOrShowDesc2('PE', '3', '1');
+  HideOrShowDesc2('PE', '1', '0');
+  HideOrShowDesc2('PE', '2', '0');
+  HideOrShowDesc2('PE', '3', '0');
 
-  HideOrShowSlideshow2('Project', '1', '1');
+  HideOrShowSlideshow2('Project', '1', '0');
 }
